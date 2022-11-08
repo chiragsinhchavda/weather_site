17b96f7416de87fa74be1ce0c3f82f21d4b9881f
iamchiragc <chiragsinhchavda01@gmail.com>
2022-11-08 23:26:01 +0530

    code review done...

--- index.js
+++ index.js
@@ -1,77 +1,97 @@
+const kelvin = 273;
+const apiKey = '56dd843e16c1a7978b1f67dede4a9ebf';
 let lat;
 let lon;
 let temp = document.getElementById('temp');
 let loc = document.getElementById('location');
 let cloud = document.getElementById('cloud');
-let table = document.querySelectorAll("table");
+let table = document.querySelectorAll('table');
 let city = document.getElementById('city');
 let state = document.getElementById('state');
+let cityErr = document.getElementById('cityValErrorMsg');
+let stateErr = document.getElementById('stateValErrorMsg');
 
 const searchData = () => {
-	let cityVal = city.value;
-	let stateVal = state.value;
-	const kelvin = 273;
-	const apiKey = '56dd843e16c1a7978b1f67dede4a9ebf';
-	const baseUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityVal}&limit=5&appid=${apiKey}`;
-	fetch(baseUrl)
-		.then(async (response) => {
-			return await response.json();
-		})
-		.then(async (data) => {
-			console.log("Data :", data);
-			data.forEach(async element => {
-				if (element.state.toUpperCase().includes(stateVal.toUpperCase())) {
-					console.log(stateVal);
-					console.log(cityVal);
-					console.log(typeof (stateVal));
-					console.log(element.state)
-					lat = element.lat;
-					lon = element.lon;
-					const latLongUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-						`lon=${lon}&appid=${apiKey}`;
-					await fetch(latLongUrl)
-						.then(async (response) => {
-							return await response.json();
-						})
-						.then((data) => {
-							console.log("Data :", data);
-							temp.innerHTML = Math.floor(data.main.temp - kelvin) + '°C';
-							loc.innerHTML = cityVal[0].toUpperCase() + cityVal.slice(1) + ', ' + element.state + ', ' + data.sys.country;
-							cloud.innerHTML = data.weather[0].description;
-							table[0].children[0].children[0].children[1].innerHTML = data.clouds.all + '%';
-							table[0].children[0].children[1].children[1].innerHTML = data.main.humidity + '%';
-							table[0].children[0].children[2].children[1].innerHTML = data.wind.speed + ' km/h';
-							table[0].children[0].children[3].children[1].innerHTML = data.main.pressure + ' hPa';
-						})
-				}
-			});
-		})
-
+	let cityValue = city.value;
+	let stateValue = state.value;
+	const urlForCityValue = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=5&appid=${apiKey}`;
+	if (cityValue == "" || stateValue == "") {
+		city.style.borderBottom = '2px solid red';
+		state.style.borderBottom = '2px solid red';
+		cityErr.innerHTML = 'City name required';
+		stateErr.innerHTML = 'State name required';
+	} else if (!isNaN(cityValue) || !isNaN(stateValue)) {
+		city.style.border = '2px solid red';
+		state.style.border = '2px solid red';
+		cityErr.innerHTML = 'City value should not a number';
+		stateErr.innerHTML = 'State value should not a number';
+	} else {
+		city.style.border = '1px solid gray';
+		state.style.border = '1px solid gray';
+		cityErr.innerHTML = '';
+		stateErr.innerHTML = '';
+		try {
+			fetch(urlForCityValue)
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					data.forEach(element => {
+						if (element.state.toUpperCase() === stateValue.toUpperCase()) {
+							state.style.border = '1px solid gray';
+							stateErr.innerHTML = '';
+							lat = element.lat;
+							lon = element.lon;
+							const latLongUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+							fetch(latLongUrl)
+								.then((response) => {
+									return response.json();
+								})
+								.then((data) => {
+									temp.innerHTML = Math.floor(data.main.temp - kelvin) + '°C';
+									loc.innerHTML = cityValue[0].toUpperCase() + cityValue.slice(1) + ', ' + element.state + ', ' + data.sys.country;
+									cloud.innerHTML = data.weather[0].description;
+									table[0].children[0].children[0].children[1].innerHTML = data.clouds.all + '%';
+									table[0].children[0].children[1].children[1].innerHTML = data.main.humidity + '%';
+									table[0].children[0].children[2].children[1].innerHTML = data.wind.speed + ' km/h';
+									table[0].children[0].children[3].children[1].innerHTML = data.main.pressure + ' hPa';
+								})
+						}
+					});
+				})
+		} catch (errorMsg) {
+			window.alert(errorMsg.message);
+		}
+	}
 }
 
 const getWeather = () => {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(async (position) => {
-			const kelvin = 273;
+		navigator.geolocation.getCurrentPosition((position) => {
 			lat = position.coords.latitude;
 			lon = position.coords.longitude;
-			const apiKey = '56dd843e16c1a7978b1f67dede4a9ebf';
-			const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+			const urlForLatLong = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
 				`lon=${lon}&appid=${apiKey}`;
-			await fetch(url)
-				.then(async (response) => {
-					return await response.json();
-				})
-				.then((data) => {
-					temp.innerHTML = Math.floor(data.main.temp - kelvin) + '°C';
-					loc.innerHTML = data.name + ', ' + data.sys.country;
-					cloud.innerHTML = data.weather[0].description;
-					table[0].children[0].children[0].children[1].innerHTML = data.clouds.all + '%';
-					table[0].children[0].children[1].children[1].innerHTML = data.main.humidity + '%';
-					table[0].children[0].children[2].children[1].innerHTML = data.wind.speed + ' km/h';
-					table[0].children[0].children[3].children[1].innerHTML = data.main.pressure + ' hPa';
-				})
+			try {
+				fetch(urlForLatLong)
+					.then((response) => {
+						return response.json();
+					})
+					.then((data) => {
+						temp.innerHTML = Math.floor(data.main.temp - kelvin) + '°C';
+						loc.innerHTML = data.name + ', ' + data.sys.country;
+						cloud.innerHTML = data.weather[0].description;
+						table[0].children[0].children[0].children[1].innerHTML = data.clouds.all + '%';
+						table[0].children[0].children[1].children[1].innerHTML = data.main.humidity + '%';
+						table[0].children[0].children[2].children[1].innerHTML = data.wind.speed + ' km/h';
+						table[0].children[0].children[3].children[1].innerHTML = data.main.pressure + ' hPa';
+					})
+			} catch (errorMsg) {
+				document.body.innerHTML = errorMsg.message;
+			}
 		})
+	} else {
+		window.alert('You have to give location permission for getting weather details...');
 	}
 }
 
